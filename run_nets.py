3a702bdcfb1bb8fb1fa26aa74c427e430eb2e2ff
Tim <timothysheehanc@gmail.com>
2017-12-06 12:06:57 -0800

    to run on server

--- run_nets.py
+++ run_nets.py
@@ -4,19 +4,43 @@ import numpy as np
 from matplotlib import pyplot as plt
 import cPickle as pickle
 
+change_bottleneck_layer = False
+change_small_worldness = True
 # import data
 training_data, validation_data, test_data,train_ae,val_ae,test_ae = mnist_loader.load_data_wrapper(make_binary =True)
 
 # run nets
 dat = []
-net = network.Network([784,30,10,30,784], cost=network.QuadraticCost,doHeavy = False,doSmallWorld=True,add_rand=10)
+disc = []
+net = network.Network([784,30,10,30,784], cost=network.QuadraticCost,doHeavy = False,doSmallWorld=False)
 net.large_weight_initializer()
-net.SGD(train_ae, 5, 5, 0.5,evaluation_data=val_ae) # 10, 10, 0.5
+net.SGD(train_ae, 5, 10, 0.5,evaluation_data=val_ae) # 10, 10, 0.5
 dat.append([net.weights,net.biases])
-           
-net = network.Network([784,30,10,30,784], cost=network.QuadraticCost_withEnergy,doHeavy=False,doSmallWorld=True,add_rand=10,lamb=0.01)
-net.large_weight_initializer()
-net.SGD(train_ae, 5, 5, 0.5,evaluation_data=val_ae)
-
-dat.append([net.weights,net.biases])
-pickle.dump(dat, open( "tmp_weights.p", "wb" ))
+disc += ['OG']
+if change_bottleneck_layer:
+    n_bottle = [2,5,10,20,30]
+    for i in range(len(bottle)):
+        # normal
+        this_lay = n_bottle[i]
+        net = network.Network([784,30,this_lay,30,784], cost=network.QuadraticCost,doHeavy = False,doSmallWorld=False)
+        net.large_weight_initializer()
+        net.SGD(train_ae, 5, 10, 0.5,evaluation_data=val_ae) # 10, 10, 0.5
+        dat.append([net.weights,net.biases])
+        disc += ['Bottle:' + str(this_lay)]
+        # change random cnx
+        net = network.Network([784,30,this_lay,30,784], cost=network.QuadraticCost,doHeavy = False,doSmallWorld=True,add_rand=10)
+        net.large_weight_initializer()
+        net.SGD(train_ae, 5, 10, 0.5,evaluation_data=val_ae) # 10, 10, 0.5
+        dat.append([net.weights,net.biases])
+        disc += ['Bottle (SW):' + str(this_lay)]
+        pickle.dump((dat,disc), open( "bottle_weights.p", "wb" ))
+    
+if change_small_worldness:
+    pct_try = [1,5,10,15,20]
+    for i in range(len(pct_try)):
+        net = network.Network([784,30,10,30,784], cost=network.QuadraticCost,doHeavy = False,doSmallWorld=True,add_rand = pct_try[i])
+        net.large_weight_initializer()
+        net.SGD(train_ae, 5, 10, 0.5,evaluation_data=val_ae) # 10, 10, 0.5
+        dat.append([net.weights,net.biases])
+        disc += ['Small World:' + str(pct_try)]
+    pickle.dump((dat,disc), open( "small_world.p", "wb" ))
