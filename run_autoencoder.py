92d3a9d01ba1c1d6a0634d133a77637363b8fcf7
tmtadros24 <timothy.m.tadros.17@dartmouth.edu>
2017-12-05 16:05:36 -0800

    Updated files - network.py that i use is still on my comp

--- run_autoencoder.py
+++ run_autoencoder.py
@@ -24,33 +24,25 @@ def generate_spike_trains(D, T, dt):
 training_data, validation_data, test_data,train_ae,test_ae = mnist_loader.load_data_wrapper()
 spike_trains = generate_spike_trains(test_ae[0:100][:], 100, 1)
 
-''' plot spike frequency over time
-m = np.zeros(784)
-# Number of Spikes
-n=10
-plt.figure(figsize=(10, 4))
-plt.autoscale(False)
-for i in range(10):
-	m = np.zeros(784)
-	# display original
-	ax = plt.subplot(2, n, i + 1)
-	j = 0
-	while j < i*10 + 1:
-		m = np.add(m, spike_trains[1,:,j])
-		j += 1
-	print np.max(m)
-	plt.imshow(m.reshape(28,28) / 100.0, vmin=0, vmax=1)
-	plt.gray()
-	ax.get_xaxis().set_visible(False)
-	ax.get_yaxis().set_visible(False)
-plt.show()
-'''
-
 # Train autoencoder
 net_ae = network.Network([784, 30, 5, 30, 784], cost=network.QuadraticCost)
 net_ae.large_weight_initializer()
-net_ae.SGD(train_ae, 10, 10, 0.5)
+net_ae.SGD(train_ae, 10, 10, 0.5, lmbda=1.0)
 
+plt.figure(figsize=(20,4))
+for j in range(10):
+	ax = plt.subplot(2,10, j+1)
+	plt.imshow(test_ae[j][0].reshape(28,28))
+	ax.get_xaxis().set_visible(False)
+	ax.get_yaxis().set_visible(False)
+	plt.gray()
+	ax = plt.subplot(2,10,j+1+10)
+	plt.imshow(net_ae.feedforward(test_ae[j][0]).reshape(28,28))
+	plt.gray()
+	ax.get_xaxis().set_visible(False)
+	ax.get_yaxis().set_visible(False)
+
+plt.show()
 # Save weights and biases
 
 weights = net_ae.weights
@@ -59,40 +51,40 @@ biases = net_ae.biases
 pickle.dump(weights, open( "weights_normal.p", "wb" ) )
 pickle.dump(biases, open( "biases_normal.p", "wb" ) )
 
-ifnetwork = IFNetwork(100, [784, 30, 5, 30, 784], weights, biases)
-output_spikes = ifnetwork.feedforward(spike_trains[1])
-
-spike = spike_trains[1]
+spike = spike_trains[0:10][:][:]
 n=10
 
-plt.figure(figsize=(20, 4))
-plt.autoscale(False)
-for i in range(10):
+for j in range(10):
+	im = spike_trains[j][:][:]
+	ifnetwork = IFNetwork(100, [784, 30, 5, 30, 784], weights, biases)
+	output_spikes = ifnetwork.feedforward(spike_trains[j][:][:])
+	plt.figure(figsize=(20, 4))
+	plt.autoscale(False)
+	for i in range(10):
 
-	m = np.zeros(784)
-	# display original
-	ax = plt.subplot(2, n, i + 1)
-	j = 0
-	while j < i*10 + 1:
-		m = np.add(m, spike_trains[1,:,j])
-		j += 1
-	plt.imshow(m.reshape(28,28) / 100.0, vmin=0, vmax=1)
-	plt.gray()
-	ax.get_xaxis().set_visible(False)
-	ax.get_yaxis().set_visible(False)
+		m = np.zeros(784)
+		# display original
+		ax = plt.subplot(2, n, i + 1)
+		k = 0
+		while k < i*10 + 1:
+			m = np.add(m, im[:,k])
+			k += 1
+		plt.imshow(m.reshape(28,28) / 100.0, vmin=0, vmax=1)
+		plt.gray()
+		ax.get_xaxis().set_visible(False)
+		ax.get_yaxis().set_visible(False)
 
 
-	m = np.zeros(784)
-	# display original
-	ax = plt.subplot(2, n, i + 1+n)
-	j = 0
-	while j < i*10 + 1:
-		m = np.add(m, output_spikes[:,j])
-		j += 1
-	print np.max(m)
-	plt.imshow(m.reshape(28,28) / 10.0, vmin=0, vmax=1)
-	plt.gray()
-	ax.get_xaxis().set_visible(False)
-	ax.get_yaxis().set_visible(False)
+		m = np.zeros(784)
+		# display original
+		ax = plt.subplot(2, n, i + 1+n)
+		k = 0
+		while k < i*10 + 1:
+			m = np.add(m, output_spikes[:,k])
+			k += 1
+		plt.imshow(m.reshape(28,28) / 100.0, vmin=0, vmax=1)
+		plt.gray()
+		ax.get_xaxis().set_visible(False)
+		ax.get_yaxis().set_visible(False)
 
-plt.show()
+	plt.show()
